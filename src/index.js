68e02f8ac91755469a5f0d97aee7aadbb161a033
Italo Fernandes <italojafernandes@hotmail.com>
2020-10-26 19:19:08 -0300

    Primeira rota - Hello World

--- src/index.js
+++ src/index.js
@@ -2,4 +2,8 @@ const express = require('express');
 
 const app = express();
 
+app.get('/projects', (request, response) => {
+  return response.json({message: 'Hello Worls'});
+});
+
 app.listen(3333);
