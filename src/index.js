44ab022a224218bd8b9865d2c8ab1eef3d85efb9
Italo Fernandes <italojafernandes@hotmail.com>
2020-10-26 23:39:36 -0300

    Os 3 principais tipos de parametros

--- src/index.js
+++ src/index.js
@@ -2,8 +2,48 @@ const express = require('express');
 
 const app = express();
 
-app.get('/', (request, response) => {
-  return response.json({message: 'Hello World'});
+app.use(express.json());
+
+app.get('/projects', (request, response) => {
+  const {title, name} = request.query;
+
+  console.log(title, name);
+
+  return response.json([
+    'Projeto 1', 
+    'Projeto 2',
+  ]);
+});
+
+app.post('/projects', (request, response) => {
+  const body = request.body;
+
+  console.log(body);
+
+  return response.json([
+    'Projeto 1', 
+    'Projeto 2', 
+    'Projeto 3',
+  ]);
+});
+
+app.put('/projects/:id', (request, response) => {
+  const {id} = request.params;
+
+  console.log(id);
+
+  return response.json([
+    'Projeto 4', 
+    'Projeto 2', 
+    'Projeto 3',
+  ]);
+});
+
+app.delete('/projects/:id', (request, response) => {
+  return response.json([ 
+    'Projeto 2', 
+    'Projeto 3',
+  ]);
 });
 
 app.listen(3333, () => {
