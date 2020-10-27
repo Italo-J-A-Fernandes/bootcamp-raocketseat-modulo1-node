cd40834877078fadeb38ba249b842ea90df193cb
Italo Fernandes <italojafernandes@hotmail.com>
2020-10-27 01:04:29 -0300

    Primeiros Middlewares

--- src/index.js
+++ src/index.js
@@ -1,11 +1,33 @@
 const express = require('express');
-const { uuid } = require('uuidv4');
+const { uuid, isUuid } = require('uuidv4');
 
 const app = express();
 
 app.use(express.json());
 
 const projects = [];
+
+function logRequests(request, response, next) {
+  const { method, url } = request;
+
+  const logLabel = `[${method.toUpperCase()}] ${url}`;
+
+  console.log(logLabel);
+
+  return next();
+}
+
+function validateOrojectsId(request, response, next) {
+  const { id } = request.params;
+
+  if(!isUuid(id)) {
+    return response.status(400).json({ error: 'Invalid project ID.' });
+  }
+
+  return next();
+}
+
+app.use(logRequests);
 
 app.get('/projects', (request, response) => {
   const { title } = request.query;
@@ -25,7 +47,7 @@ app.post('/projects', (request, response) => {
   return response.json(project);
 });
 
-app.put('/projects/:id', (request, response) => {
+app.put('/projects/:id', validateOrojectsId, (request, response) => {
   const { id } = request.params;
   const { title, owner} = request.body;
 
@@ -46,7 +68,7 @@ app.put('/projects/:id', (request, response) => {
   return response.json(project);
 });
 
-app.delete('/projects/:id', (request, response) => {
+app.delete('/projects/:id', validateOrojectsId, (request, response) => {
   const { id } = request.params;
 
   const projectIndex = projects.findIndex(project => project.id === id);
